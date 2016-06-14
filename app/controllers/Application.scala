7f1ac740788ab9952c5c95c99085ca5ab141876d
mauroeportillo@gmail.com <mauroeportillo@gmail.com>
2016-06-14 20:08:30 -0300

    Cambios en la UI

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,31 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.mvc.Result
+import play.libs.Json
+import scala.collection.mutable.ListBuffer
+import model.Project
+import model.Project
+
+
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-
+//  def index = Action {
+//    Ok(views.html.index("Your new application is ready."))
+//  }
+//
+//  public static Result libros() {
+//     response().setContentType("application/json");
+//     Collection<Libro> libros = Biblioteca.getInstance().todasLasInstancias();
+//     return ok(Json.toJson(libros));
+//    }
+        def proyectos = Action {
+        var project =  new Project
+        project.name = "Mi Primer Proyecto"
+        val json = Json.toJson(project.toString())
+        print(json)
+          Ok(Json.stringify(json))
+        }
+        
 }
--- public/javascripts/utilUi.js
+++ public/javascripts/utilUi.js
@@ -20,7 +20,6 @@ $(document).ready(function() {
 		}
 	});
 });
-
 $(document)
 		.ready(
 				function() {
