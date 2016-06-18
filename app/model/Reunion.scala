b3e950490fbe29fb43aa3c3dea24f70bb226ee6a
Diego Garcia <garcia.diego1990@gmail.com>
2016-06-17 21:19:41 -0300

    Actualización de reuniones

--- app/model/Reunion.scala
+++ app/model/Reunion.scala
@@ -5,38 +5,41 @@ import scala.collection.mutable.ListBuffer
 import java.util.{ Date, Locale }
 import java.text.DateFormat
 import java.text.DateFormat._
-//import java.text.SimpleDateFormat;
-//import java.util.Calendar;
+import java.text.SimpleDateFormat
+import java.text.SimpleDateFormat._
+
 import java.sql.Time
 
 class Reunion {
-	var fecha = null: Date
-	var horaDeInicio = null: Time
-//var horaDeFinalizacion = null: Time
-	var integrantes = new ListBuffer[Usuario]
+  var id = null: Integer
+  var fecha = null: Date
+  var horaDeInicio = null: Time
+  var integrantes = new ListBuffer[Usuario]
+  var temasTratados = null: Tema
 
-			def setFecha(dia: Integer, mes: Integer, anio: Integer) {
-	       var nvaFecha = new Date((anio - 1900), mes - 1, dia)
-				 var df = getDateInstance(LONG, Locale.ENGLISH)
-				 //println(df format fecha)
-	    }
+  def setFecha(dia: Integer, mes: Integer, anio: Integer) {
+    var nvaFecha = new Date((anio - 1900), mes - 1, dia)
+    var df = getDateInstance(LONG, Locale.ENGLISH)
+    //println(df format fecha)
+  }
 
-	    def setHora(hora: Integer, minutos: Integer) { 
-		     var nvaHora = new Time(hora, minutos,0)
-	    
-				 //println(t)
-	    }
-	    
-	    def agregarIntegrante(participante:Usuario){
-        integrantes.+=(participante)
-      }
-      
-	    def agregarTema(lista: ListBuffer[String], descripicionDelTema:String){
-        lista.+=:(descripicionDelTema)
-      }
-	    
-	    def crearReunion(dia:Integer, mes:Integer, anio:Integer, hora:Integer, minutos:Integer ){
-         this.setFecha(dia,mes,anio)
-         this.setHora(hora, minutos)
-      }
+  def setHora(hora: Integer, minutos: Integer) {
+    var nvaHora = new Time(hora, minutos, 0)
+    //println(t)
+  }
+
+  def agregarIntegrante(participante: Usuario) {
+    integrantes.+=(participante)
+  }
+
+  def agregarTema(temaNvo: String, descripcionDelTema: String) {
+    temasTratados.setDescripcionTema(descripcionDelTema)
+    temasTratados.temas.+=:(temaNvo)
+  }
+
+  def crearReunion(id: Integer, dia: Integer, mes: Integer, anio: Integer, hora: Integer, minutos: Integer) {
+    this.id = id
+    this.setFecha(dia, mes, anio)
+    this.setHora(hora, minutos)
+  }
 }
